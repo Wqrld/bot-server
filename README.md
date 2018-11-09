da767f1dd711f59ebfe2624068e05277597a64e0
Ferox <wereld03@gmail.com>
2018-11-09 21:43:50 +0100

    cleanup and a readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minecraft faction bot server
+The backend part of a bot that i used on many factions and skyblock servers, this thing can use quite some cpu but runs fine on a hetzner or ovh vps.
+This was never built to be opensource so the code can be quite messy
+
+Things to do yourself:
+- accounts.json 
+- user.htpasswd 
--- cactus.js
+++ cactus.js
@@ -9,7 +9,6 @@ const fs = require("fs");
 const app = express()
 const port = 8095
 const pm2 = require('pm2')
-//const Hook = new Webhook("https://discordapp.com/api/webhooks/474143104924909580/ji_TyKsivTSHhvHWVeQQmmlND32PyiiA5PDtvylqhKBOwj8rX2-YzTOG5AdXoFZPV6x6");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -21,27 +20,7 @@ var bots = [];
 var botigns = [];
 var botnames = {};
 var botnr = 1
-/*
 
-const MongoClient = require('mongodb').MongoClient;
-async function mongotest() {
-client = await MongoClient.connect("mongodb://localhost:27017/Bots");
-const db = client.db("Bots");
-const collection = db.collection('Bots');
-
-
-
-
-collection.find({}).toArray(function(err, result) {
-//console.log(result[0].bots)
-
-});
-
-};*/
-//var joincommand = "/play factions"
-//const host = "66.70.181.76"
-//const host = "137.74.46.4"
-/////////
 var accounts = require('./accounts.json');
 var config = require('./config.json')
 
@@ -54,9 +33,6 @@ accounts.forEach(function(account) {
 
         try {
             console.log(account.name);
-            /////////
-            // Create bot
-            /////////
             botnames[account.name] = mineflayer.createBot({
                 host: config.ip,
                 port: config.port,
@@ -65,17 +41,11 @@ accounts.forEach(function(account) {
                 password: account.password
             })
 
-          //  mongotest()
-            /////////
 
             setTimeout(function() {
-botnames[account.name].chat(config.joincommand);
+                botnames[account.name].chat(config.joincommand);
                 console.log(`spawned and did ${config.joincommand}`)
             }, 3000);
-
-                
-         
-
 
             console.log(account.name + " init")
             botigns.push(account.name);
@@ -120,139 +90,13 @@ setTimeout(function() {
 }, 12000);
 
 
-
-
-/*
-function joinpig() {
-    Pig = mineflayer.createBot({
-        host: host,
-        username: "FearTehPig",
-        version: "1.9",
-    })
-    console.log("1. pig init");
-
-}
-
-function joinwater() {
-    Water = mineflayer.createBot({
-        host: host,
-        username: "w.ereld03@gmail.com",
-        version: "1.9",
-    })
-    console.log("2. water init");
-    
-    //console.log(bots);
-}
-
-function joinstijn() {
-    var Stijn = mineflayer.createBot({
-        host: host,
-        username: "stijn.boeren@hotmail.nl",
-        version: "1.9",
-    })
-    console.log("3. stijn init");
-    bots = [Water, Pig, Stijn]
-        botnames = {"Waterwerse": Water, "stijn31231": Stijn, "FearTehPig": Pig}
-    console.log("x. list init");
-            Water.on('message', (message) => {
-            console.log(message);
-io.emit('chatc', {"message": message.toAnsi(), "username": "Waterwerse"});
-});
-
-}*/
-//joinpig()
-//setTimeout(joinwater, 5000);
-//setTimeout(joinstijn, 12000);
-
-//console.log(Stijn)
-
-
-
-//Pig.username = "FearTehPig"
-//Water.username = "Waterwerse"
-/*
-function initbotloop() {
-    bots.forEach(function(bot) {
-       
-
-        bot.once('spawn', () => {
-            //bot.chat('/play factions');
-            //bot.quit()
-            bot.chat("hey");
-        });
-
-        bot.on('message', (message) => {
-            console.log(message);
-            io.emit('chatc', {
-                "message": message.toAnsi(),
-                "username": bot.username
-            });
-
-
-            try {
-                if (message.extra[2] !== undefined) {
-
-                    if (
-
-                        message.extra[2].text.indexOf("+") !== -1
-
-                    ) {
-                        console.log("[inttest]" + parseInt(message.extra[2].text) + "[delim]");
-                        console.log("[text] " + message.extra[2].text);
-                        try {
-                            var arr = message.extra[2].text.split(" + ")
-                            var i = parseInt(arr[0]) + parseInt(arr[1])
-                            console.log("[nr]" + i);
-                            io.emit('number', i);
-                            //Pig.chat(i);
-                        } catch (err) {
-
-                            console.log(err)
-                        }
-                    }
-                }
-
-
-
-            } catch (err) {
-                //console.log(err);
-            }
-            if (message.json.hoverEvent !== undefined) {
-                console.log(message.json.hoverEvent.value[0].text);
-                // Pig.chat(message.json.hoverEvent.value[0].text);
-            }
-            /* if(message.extra[1].text == 'Solve'){
-            //    console.log("%c" + message.extra[2].text, "color:" + Orange);
-                console.log('\x1b[33m%s\x1b[0m', message.extra[2].text);  //yellow
-            }
-            if(message.extra[1].text == 'Hover for the word to type!'){
-
-console.log('\x1b[33m%s\x1b[0m', message.json.hoverEvent.value);
-            }
-
-
-        })
-
-
-    });
-
-}
-*/
-//setTimeout(initbotloop, 4000);
-//initbotloop()
-
 rl.on('line', (input) => {
     if (input == "block") {
         bots.forEach(function(bot) {
             console.log(bot.blockAt(bot.entity.position.offset(0, -1, 0)).type);
         });
 
-
-
     }
-
-
-
 
     if (input == "join") {
         bots.forEach(function(bot) {
@@ -262,20 +106,7 @@ rl.on('line', (input) => {
 
 
 });
-//if (input == "raided"){
-//console.log(bot.blockAt(v(9602, 250, 9602)));
-//}else{
-//bot.chat(input);
-//}
 
-function joinfacs() {
-    bots.forEach(function(bot) {
-        bot.chat("/server skyblockice");
-    });
-}
-
-
-//setInterval(joinfacs, 90000);
 
 function checkraid() {
     try {
@@ -289,7 +120,6 @@ function checkraid() {
 
 
 }
-//setInterval(checkraid, 9000);
 
 function initwebsocket() {
     console.log("initting websockets")
@@ -330,7 +160,6 @@ function initwebsocket() {
                 }
 
             }
-            //  console.log(botnames);
         });
 
         socket.on('setip', function(msg) {
@@ -375,5 +204,4 @@ function initwebsocket() {
     });
 }
 initwebsocket()
-//setTimeout(initwebsocket, 4000);
 app.listen(port, () => console.log(`Api server listening on port ${port}!`))
